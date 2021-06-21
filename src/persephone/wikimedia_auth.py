c956b24192742a913f556ce229a3a8f204bf18f3
Christos Alexiou <pcnoic@gmail.com>
2021-06-21 13:37:13 +0300

    Stripped down wiki verify

--- src/persephone/wikimedia_auth.py
+++ src/persephone/wikimedia_auth.py
@@ -2,6 +2,7 @@
     OAuth owner-only consumers authenticating against WikiPedia's API
 """
 
+from fastapi import responses
 from fastapi_users import user
 import requests
 from config import ConfigParams
@@ -9,56 +10,65 @@ from config import ConfigParams
 
 class WikimediaAuth:
 
-    """
-        Fetches an API action token.
-        Most common token type: login
-        See more: https://www.mediawiki.org/wiki/API:Tokens
-    """
-    def get_API_token(self):
-        S = requests.session()
-        URL = ConfigParams.WIKIPEDIA_API_URL
+    S = requests.Session()
+    URL = ConfigParams.WIKIPEDIA_API_URL
 
-        PARAMS = {
-            "action":"query",
-            "meta": "tokens",
-            "type": "login",
-            "format": "json"
-        }
+    def start_client_login(self, username, password):
+        """
+            Send a post request along with login token,
+            user information and return URL to the API 
+            to log in on a wiki
+        """
+        login_token = self.fetch_login_token()
+        session = self.S
 
-        req = S.get(url=URL, params=PARAMS)
-        res = req.json()
-
-        return res["query"]["tokens"]["logintoken"]
-
-    """
-        Sending post request to login.
-        Requires API:Token of type 'login'
-    """
-    def get_login(self, username, password, token):
-        S = requests.Session()
-        URL = ConfigParams.WIKIPEDIA_API_URL
-
-        PARAMS = {
-            "action":"login",
-            "lgname":username,
-            "lgpassword": password,
-            "lgtoken":token,
-            "format":"json"
-        }
-
-        req = S.post(URL, data=PARAMS)
-        res = req.json()
+        response = session.post(url=self.URL, data={
+            'action': "clientlogin",
+            'username': username,
+            'password': password,
+            'loginreturnurl': "http://localhost:8000/",
+            'logintoken': login_token,
+            'format': "json"
+        })
         
-        print(res)
-        return res
+        data = response.json()
+        
+        if data['clientlogin']['status'] == 'PASS':
+            # Handle success
+            print("Successful login for WikiPedia user {user}".format(
+                user = data['clientlogin']['username']
+            ))
+            return data['clientlogin']['username']
+        else:
+            # Handle failure
+            print("Something went wrong when trying to login with {user} and {password}".format(
+                user = username,
+                password = password
+            ))
+            return 1
 
+    def fetch_login_token(self):
+        """ Fetch login token via 'tokens' module """
+        session = self.S
+        response = session.get(
+            url=self.URL,
+            params={
+                'action': "query",
+                'meta': "tokens",
+                'type': "login",
+                'format': "json"
+                }
+        )
+        data = response.json()
+        token = data['query']['tokens']['logintoken']
+        print("Received token " + token) 
+        return token
 
 """
     Identity Verifier
 """
 def verify_wikiId(username, password):
     wikimedia_auth = WikimediaAuth()
-    login_token = wikimedia_auth.get_API_token()
-    login_res = wikimedia_auth.get_login(username, password, login_token)
-    print(login_res)
+    wiki_user = wikimedia_auth.start_client_login(username, password)
+    print(wiki_user)
         
