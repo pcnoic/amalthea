cbe62835c85b2d80d3290f5f6be17d37f1855606
Christos Alexiou <pcnoic@gmail.com>
2021-07-03 18:02:04 +0300

    packages: ipfs

--- src/estia/fs.py
+++ src/estia/fs.py
@@ -1,10 +1,9 @@
 """
     Interacting with the IPFS API. 
 """
-import ipfsApi
+import ipfs-api
 from config import ConfigParams
 
 def get_api_con():
     api = ipfsApi.Client(ConfigParams.IPFS_API, ConfigParams.IPFS_PORT)
     return api
-
